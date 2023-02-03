c976e7ba31b1392b02607bd46491b32e5fb2fa25
Virginio Carfagno <virginio.carfagno@mindmatters.de>
2023-02-03 09:39:32 +0100

    Increment counts on click

--- src/demo.tsx
+++ src/demo.tsx
@@ -3,6 +3,7 @@ import Badge, { BadgeProps } from "@mui/material/Badge";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
+import { useState } from "react";
 
 const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
   "& .MuiBadge-badge": {
@@ -15,18 +16,22 @@ const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
 }));
 
 export default function CustomizedBadges() {
+  const [managerCount, setManagerCount] = useState(97);
+  const [openCount, setOpenCount] = useState(0);
+
   return (
     <Box sx={{ display: "flex", gap: 2 }}>
       <Link
         href="#"
-        onClick={() => console.log("clicked")}
+        onClick={() => setManagerCount(managerCount + 1)}
         sx={{ paddingLeft: "30px" }}
         underline="none"
         variant="body1"
       >
         <StyledBadge
-          badgeContent={222}
+          badgeContent={managerCount}
           color="secondary"
+          showZero
           anchorOrigin={{
             horizontal: "left",
             vertical: "top"
@@ -36,14 +41,15 @@ export default function CustomizedBadges() {
       </Link>
       <Link
         href="#"
-        onClick={() => console.log("clicked")}
+        onClick={() => setOpenCount(openCount + 1)}
         sx={{ paddingLeft: "30px", paddingRight: "10px" }}
         underline="none"
         variant="body1"
       >
         <StyledBadge
-          badgeContent={2}
+          badgeContent={openCount}
           color="secondary"
+          showZero
           anchorOrigin={{
             horizontal: "left",
             vertical: "top"
